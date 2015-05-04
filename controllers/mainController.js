cd3feb4d563f0b41817208a203b268c2e18ac147
My Name <aaronollis121@gmail.com>
2015-05-03 17:04:29 -0700

    error fixes

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -9,6 +9,32 @@ controllers.mainController = function ($scope) {
 		{name: 'Heedy Wahlin', city: 'San Francisco' },
 		{name: 'Thomas Winter', city: 'Santa Barbara' }
 	];
-}; 
+
+	$scope.addCustomer = function () {
+		$scope.customers.push(
+			{
+				name: $scope.newCustomer.name,
+				city: $scope.newCustomer.city
+			});
+	}; 
+}
 
 customerApp.controller(controllers);
+
+//routes that will be connected later//
+
+// demoApp.config(function ($routeProvider) {
+// 	$routeProvider
+// 		.when('/',
+// 			{
+// 				controller: 'mainController',
+// 				templateUrl: 'view1.html'
+// 			})
+// 		.when('/partial2',
+// 			{
+// 				controller: 'mainController',
+// 				templateUrl: 'view2.html'
+// 			})
+// 		.otherwise({ redirectTo: '/' });
+			
+// });
