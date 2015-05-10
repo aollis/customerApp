bbf501abc71bc9d29de5b36211d58fbbf542a84d
My Name <aaronollis121@gmail.com>
2015-05-09 19:15:42 -0700

    routing work

--- controllers/app.js
+++ controllers/app.js
@@ -1,35 +1,35 @@
-var customerApp = angular.module('customerApp', ['ngRoute']);
+		var customerApp = angular.module('customerApp', ['ngRoute']);
 
-customerApp.config(function ($routeProvider) {
-	$routeProvider
-	    .when('/index', route.resolve('view1'))
-	    .when('/view2', route.resolve('view2'))
-	    .otherwise({ redirectTo: '/index' });
-			
-});
+		customerApp.config(function($routeProvider) {
+			$routeProvider
+ 				.when('/', {
+                    templateUrl: '/views/view1.html',
+                    controller: 'mainController'
+                })
+                .when('/view2', {
+                    templateUrl: '/views/view2.html',
+                    controller: 'mainController'
+                })
+                .otherwise({
+                    redirectTo: '/views/view1.html'
+                });
 
+        });
 
-customerApp.controller('mainController', function ($scope) {
-	$scope.customers = [
-		{name: 'David Jones', city: 'New York' },
-		{name: 'Jamie Riley', city: 'Boston' },
-		{name: 'Heedy Wahlin', city: 'San Francisco' },
-		{name: 'Thomas Winter', city: 'Santa Barbara' }
-	];
+		customerApp.controller('mainController', function($scope) {
+			$scope.customers = [
+				{name: 'David Jones', city: 'New York' },
+				{name: 'Jamie Riley', city: 'Boston' },
+				{name: 'Heedy Wahlin', city: 'San Francisco' },
+				{name: 'Thomas Winter', city: 'Santa Barbara' }
+			];
 
-	// $scope.addCustomer = function ($scope) {
-	// 	$scope.customers.push(
-	// 		{
-	// 			name: $scope.newCustomer.name,
-	// 			city: $scope.newCustomer.city
-	// 		});
-	// }; 
+			$scope.addCustomer = function() {
+				$scope.customers.push(
+					{
+						name: $scope.newCustomer.name,
+						city: $scope.newCustomer.city
+					});
+			}; 
 
-});
-
-
-
-
-
-
-
+		});
