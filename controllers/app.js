2843360ce481768a4465c2e6a736eab5247252d6
aaronollis <aaronollis121@gmail.com>
2015-06-12 13:36:44 -0700

    cleaned up syntax, bower components added

--- controllers/app.js
+++ controllers/app.js
@@ -1,36 +1,36 @@
-		var customerApp = angular.module('customerApp', ['ngRoute']);
+var customerApp = angular.module('customerApp', ['ngRoute']);
 
-		customerApp.config(function($routeProvider) {
-			$routeProvider
- 				.when('/angularPractice/angularPractice/index.html', {
-                    templateUrl: '/angularPractice/angularPractice/views/view1.html',
-                    controller: 'mainController'
-                })
-                .when('/angularPractice/angularPractice/views/view2', {
-                    templateUrl: '/angularPractice/angularPractice/views/view2.html',
-                    controller: 'mainController'
-                })
-                .otherwise({
-                    redirectTo: '/angularPractice/angularPractice/views/view1.html',
-                    controller: 'mainController'
-                });
+customerApp.config(function($routeProvider) {
+	$routeProvider
+ 		.when('/angularPractice/angularPractice/index.html', {
+         	templateUrl: '/angularPractice/angularPractice/views/view1.html',
+            controller: 'mainController'
+            })
+        .when('/angularPractice/angularPractice/views/view2', {
+        templateUrl: '/angularPractice/angularPractice/views/view2.html',
+        controller: 'mainController'
+            })
+        .otherwise({
+            redirectTo: '/angularPractice/angularPractice/views/view1.html',
+            controller: 'mainController'
+            });
 
         });
 
-		customerApp.controller('mainController', function($scope) {
-			$scope.customers = [
-				{name: 'David Jones', city: 'New York' },
-				{name: 'Jamie Riley', city: 'Boston' },
-				{name: 'Heedy Wahlin', city: 'San Francisco' },
-				{name: 'Thomas Winter', city: 'Santa Barbara' }
-			];
+customerApp.controller('mainController', function($scope) {
+	$scope.customers = [
+		{name: 'David Jones', city: 'New York' },
+		{name: 'Jamie Riley', city: 'Boston' },
+		{name: 'Heedy Wahlin', city: 'San Francisco' },
+		{name: 'Thomas Winter', city: 'Santa Barbara' }
+		];
 
-			$scope.addCustomer = function() {
-				$scope.customers.push(
-					{
-						name: $scope.newCustomer.name,
-						city: $scope.newCustomer.city
-					});
-			}; 
-
+	$scope.addCustomer = function() {
+		$scope.customers.push(
+			{
+				name: $scope.newCustomer.name,
+				city: $scope.newCustomer.city
 		});
+	}; 
+
+	});
